53e38b4903154f5334fdddde50ffabcdde8ea420
jordig15 <jordi.guitart02@estudiant.upf.edu>
2021-05-04 22:46:30 +0200

    commit

---
+++ Introduction to Scripting Part 1/Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public float runSpeed; // 1
+    public float gotHayDestroyDelay; // 2
+    private bool hitByHay; // 3
+
+    public float dropDestroyDelay; // 1
+    private Collider myCollider; // 2
+    private Rigidbody myRigidbody; // 3
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.back * runSpeed, Space.World);
+    }
+
+    private void HitByHay()
+    {
+        hitByHay = true; // 1
+        runSpeed = 0; // 2
+
+        Destroy(gameObject, gotHayDestroyDelay); // 3
+    }
+
+    private void OnTriggerEnter(Collider other) // 1
+    {
+        if (other.CompareTag("Hay") && !hitByHay) // 2
+        {
+            Destroy(other.gameObject); // 3
+            HitByHay(); // 4
+        }
+        else if (other.CompareTag("DropSheep"))
+        {
+            Drop();
+        }
+    }
+    private void Drop()
+    {
+        myRigidbody.isKinematic = false; // 1
+        myCollider.isTrigger = false; // 2
+        Destroy(gameObject, dropDestroyDelay); // 3
+    }
+}
